f6793ad40e848049d1c4dfe52f4089d03ec94733
rusavcik <rusavchik@gmail.com>
2023-05-18 20:08:50 +0300

    Try to fix...

--- ping-pong.py
+++ ping-pong.py
@@ -1,17 +1,16 @@
-from livewires import games, color
-import random
+from superwires import games, color
 
 games.init(screen_width = 437, screen_height = 780, fps = 50)
 
 
+# create player object
 class Player(games.Sprite):
-
     image = games.load_image("rocket.bmp")
-    
+
     def __init__(self):
-        super(Player, self).__init__(image = Player.image,
-                                     x = games.mouse.x,
-                                     bottom = games.screen.height)
+        super(Player, self).__init__(image=Player.image,
+                                     x=games.mouse.x,
+                                     bottom=games.screen.height)
 
     def update(self):
         self.x = games.mouse.x
@@ -19,20 +18,21 @@ class Player(games.Sprite):
             self.left = 0
         if self.right > games.screen.width:
             self.right = games.screen.width
-            
 
+
+# create ball object
 class Ball(games.Sprite):
-
     image = games.load_image("ball.bmp")
-    
+
     speed = 2
 
     def __init__(self):
-        super(Ball, self).__init__(image = Ball.image,
-                                    x = games.screen.width / 2,
-                                    top = 0,
-                                    dx = Ball.speed,
-                                    dy = Ball.speed)
+        super(Ball, self).__init__(image=Ball.image,
+                                   x=games.screen.width/2,
+                                   y=0,
+                                   top=0,
+                                   dx=Ball.speed,
+                                   dy=Ball.speed)
 
     def update(self):
         if self.left < 0 or self.right > games.screen.width:
@@ -49,27 +49,27 @@ class Ball(games.Sprite):
             self.destroy()
 
     def end_game(self):
-        end_message = games.Message(value = "Game Over",
-                                    size = 90,
-                                    color = color.red,
-                                    x = games.screen.width/2,
-                                    y = games.screen.height/2,
-                                    lifetime = 5 * games.screen.fps,
-                                    after_death = games.screen.quit)
+        end_message = games.Message(value="Game Over",
+                                    size=90,
+                                    color=color.red,
+                                    x=games.screen.width / 2,
+                                    y=games.screen.height / 2,
+                                    lifetime=5 * games.screen.fps,
+                                    after_death=games.screen.quit)
         games.screen.add(end_message)
 
 
 def main():
-    wall_image = games.load_image("table.jpg", transparent = False)
+    wall_image = games.load_image("table.jpg", transparent=False)
     games.screen.background = wall_image
-    
+
     the_player = Player()
     games.screen.add(the_player)
 
     the_ball = Ball()
     games.screen.add(the_ball)
 
-    games.mouse.is_visible = False
+    games.mouse._is_visible = False
 
     games.screen.event_grab = True
 
